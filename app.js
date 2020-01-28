413825b82cfdf10cb806fb6c55ff37fcdea1ee67
shina07 <limtsoo@kaist.ac.kr>
2020-01-28 23:09:50 +0900

    Implement DB Manager

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Modules
-var express = require('express');
 // var http = require('http');
+var express = require('express');
 var path = require('path');
 
-var favicon = require('serve-favicon')
+var favicon = require('serve-favicon');
+var session = require('express-session');
+
 // All Routers
 var index = require('./routes/index');
 var home = require('./routes/home')
@@ -18,21 +20,27 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use(favicon(path.join(__dirname, 'public/image', 'favicon.ico')))
+app.use(session({	resave: true,
+					saveUninitialized: true,
+					secret: 'secret' }));
+
 app.use('/', index);
 app.use('/home', home)
 app.use('/login', login);
 app.use('/ranking', ranking);
 app.use('/test', test);
-app.use(express.static(path.join(__dirname, '/public')));
 
-// Create Server (Node 3.x and under)
+// Create server instance
 // var server = http.createServer(app);
 // sever.listen(app.get('port'), function() {
 // 	console.log("Express Server has started on port " + app.get('port'))
 // });
 
-// Node 4.x and higher does not need http module
-app.listen(app.get('port'), function() {
+// Express create server (app.listen also returns server instance)
+var server = app.listen(app.get('port'), function() {
 	console.log("Express Server listening on port " + app.get('port'))
 });
---
+++ dbmanager.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+var db_config = require('./config.json').db_config;
+
+// MySQL Connection Setting
+var connection = mysql.createConnection({
+	host: db_config.host,
+	port: db_config.port,
+	user: db_config.user,
+	password: db_config.password,
+	database: db_config.database
+});
+
+connection.connect(function(erorr) {
+	if (error) {
+		console.log("ERROR Ocurred", error);
+		throw error;
+	};
+});
+
+module.exports = {
+
+};
